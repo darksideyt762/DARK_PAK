9a535156a9542725e5c22c17a9ff6ac59c474261
darksideyt762 <darksidegaming762@gmail.com>
2025-03-05 00:45:28 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to the DARK_PAK folder inside Termux
+cd ~/DARK_PAK || { echo "DARK_PAK folder not found! Cloning..."; git clone https://github.com/darksideyt762/DARK_PAK ~/DARK_PAK && cd ~/DARK_PAK; }
+
+# Make sure the starter script is executable
+chmod +x skintool
+
+# Run the starter script
+./skintool
