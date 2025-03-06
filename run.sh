04540b8fb71f4bac568566c6d66957e60a9a30c0
darksideyt762 <darksidegaming762@gmail.com>
2025-03-06 08:47:17 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # Navigate to the DARK_PAK folder inside Termux
-cd ~/DARK_PAK || { echo "DARK_PAK folder not found! Cloning..."; git clone https://github.com/darksideyt762/DARK_PAK ~/DARK_PAK && cd ~/DARK_PAK; }
+cd ~/DARK_PAK || { 
+    echo "DARK_PAK folder not found! Cloning..."; 
+    git clone https://github.com/darksideyt762/DARK_PAK ~/DARK_PAK && cd ~/DARK_PAK; 
+}
 
 # Make sure the starter script is executable
 chmod +x skintool
+
+# Clear the screen before running the tool
+clear
 
 # Run the starter script
 ./skintool
